520fc3c7348f65f45032d00fd027e9c2c0c39b32
Andrew Ferkel <142550874+AFERKEL@users.noreply.github.com>
2024-09-01 20:23:25 -0500

    Created FileNo1

---
+++ FileNo1.py
@@ -0,0 +1,18 @@
+Holidays = {
+    "New Year's Day" : (1,-1,1,1), 
+    "Martin Luther King Day" : (1,0,15,21),
+    "Washington's Birthday" : (5,0,15,21),
+    "Memorial Day" : (5,0,25,31),
+    "Juneteenth National Independence Day" : (6,-1,19,19),
+    "Independence Day" : (7,-1,4,4),
+    "Labor Day" : (9,-1,1,7),
+    "Columbus Day" : (10,0,8,14),
+    "Halloween" : (10 ,-1,31,31),
+    "Veteran's Day" : (11,-1,11,11),
+    "Thanksgiving" : (11,3,22,28),
+    "Christmas Eve" : (12,-1,24,24),
+    "Christmas Day" : (12,-1,25,25),
+    "New Year’s Eve" : (12,-1,31,31)
+}
+
+print(Holidays)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 2024.09.01 Individual Assigntment 1
+# 2024.09.01 Individual Assignment 1
 2024.09.01 - Repository for the Individual Assignment 1. 
